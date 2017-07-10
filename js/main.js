d3a7674d6956fce73710172f15c93a1f4e84e612
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-07-09 21:04:51 -0400

    Cannot play on used square, start button reveals game board

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var boardSquares = document.querySelectorAll('td.square');
+var board = document.querySelector("div#board")
 var player = 1
+var startButton = document.querySelector("button.start")
 
+startButton.addEventListener('click', function(){
+  board.style.display = "block";
+})
 boardSquares.forEach(function(square, index){
 
 square.style.backgroundColor = "purple";
+
+
 square.addEventListener('click', function(){
 
-  if (player === 1){
+
+  if (square.innerHTML === ""){
+   if (player === 1){
   var skull = document.createElement('img');
-  skull.src = "img/skull2.jpg";
+  skull.src = 'img/skull2.jpg';
   square.append(skull);
   return player = 2;
 } else if(player === 2){
 var bones = document.createElement('img');
-bones.src = "img/bones.jpg";
+bones.src = 'img/bones.jpg';
 square.append(bones);
 return player = 1;
+}
+} else {
+  return alert("ARRGH!!! That Block is TAKEN!!");
 }
 
 
