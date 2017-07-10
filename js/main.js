da6bf00b42233423585ad1b3ce9c864e55bd9688
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-07-09 20:13:07 -0400

    Able to append skull to board

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var boardSquares = document.querySelectorAll('td.square');
-var colors = ["purple","blue","red"]
+
+
 boardSquares.forEach(function(square, index){
-  c = Math.ceil(Math.random() * 3);
+
 square.style.backgroundColor = "purple";
 square.addEventListener('click', function(){
-  square.style.backgroundColor = "red";
+  // square.style.backgroundColor = "red";
+  var skull = document.createElement('img');
+  skull.src = "img/skull2.jpg";
+  square.append(skull);
 })
 
 })
