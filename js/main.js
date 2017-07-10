9d73b118ba59196e2887370ec5fc017fffd8ad1b
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-07-09 20:30:49 -0400

    Crossbones working as x image alternates between player one and player 2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var boardSquares = document.querySelectorAll('td.square');
-
+var player = 1
 
 boardSquares.forEach(function(square, index){
 
 square.style.backgroundColor = "purple";
 square.addEventListener('click', function(){
-  // square.style.backgroundColor = "red";
+
+  if (player === 1){
   var skull = document.createElement('img');
   skull.src = "img/skull2.jpg";
   square.append(skull);
+  return player = 2;
+} else if(player === 2){
+var bones = document.createElement('img');
+bones.src = "img/bones.jpg";
+square.append(bones);
+return player = 1;
+}
+
+
+
+
 })
 
 })
