d6503c5993901d946f4227fe0b364fd679520d70
Vlad Wolanyk <vladwolanyk@gmail.com>
2019-06-06 18:22:37 -0400

    fix er upper

--- js/main.js
+++ js/main.js
@@ -1,100 +1,162 @@
-// document.addEventListener('DOMContentLoaded', function(){
-//
-// // Def initial variables
-// var resetButton = document.querySelector("button.reset");
-// var moveCount = 0;
-// var player = 1;
-//
-// var row1var = document.getElementById("row1").children;
-// var row2var = document.getElementById("row2").children;
-// var row3var = document.getElementById("row3").children;
-// var column1 = [row1var[0],row2var[0],row3var[0]];
-// var column2 = [row1var[1],row2var[1],row3var[1]];
-// var column3 = [row1var[2],row2var[2],row3var[2]];
-// var diagLeft = [row1var[0],row2var[1],row3var[2]];
-// var diagRight = [row3var[0],row2var[1],row1var[2]];
-// var winPossibilities = [row1var,row2var,row3var, column1, column2, column3, diagLeft, diagRight];
-//
-//
-//
-// // Game Method? Use this or no? Called when start button clicked
-// function newGame(){
-// startButton.innerHTML = "New Game";
-//   // Game Variables
-//   var boardSquares = document.querySelectorAll('td.square');
-//   var board = document.querySelector("div#board");
-//   var playerTurn = document.querySelector("h2.player");
-//   var moves = document.querySelector("h2.moves");
-//   var win = false;
-//
-//   // Reveal the Board - Initially Invisible
-//   board.style.display = "block";
-//
-//   // Adds A line on top of board that identifies who's turn it is and how many moves
-//   playerTurn.innerHTML = "It's Yer Move Player: " + player;
-//   moveCount = 0;
-//   moves.innerHTML = "Total Moves: " + moveCount;
-//
-//   // Iterates over each square in game board
-//   boardSquares.forEach(function(square, index){
-//     square.innerHTML = "";
-//     square.removeAttribute("data-value");
-//     // Adds a number to square(for the formula to determine WIN)
-//     square.classList.add("sq"+(index+1));
-//     // Styling - each board square purple(could add to CSS)
-//     square.style.backgroundColor = "purple";
-//
-// // Click HAndler function for board squares
-// clickHandlerBoard = function(e){
-//     e.stopPropagation;
-//
-//       if (square.innerHTML === "" ){
-//           if (player === 1){
-//             var skull = document.createElement('img');
-//             skull.src = 'img/skull2.jpg';
-//             square.setAttribute("data-value","skull")
-//             square.append(skull);
-//             square.removeEventListener('click', clickHandlerBoard);
-//             moveCount++;
-//             moves.innerHTML = "Total Moves: " + moveCount;
-//             player = 2;
-//             playerTurn.innerHTML = "It's Yer Move Player: " + player;
-//             } else if(player === 2){
-//           var bones = document.createElement('img');
-//           bones.src = 'img/bones.jpg';
-//           square.setAttribute("data-value","bones")
-//           square.append(bones);
-//           square.removeEventListener('click',clickHandlerBoard);
-//           moveCount++
-//           moves.innerHTML = "Total Moves: " + moveCount;
-//           player = 1;
-//           playerTurn.innerHTML = "It's Yer Move Player: " + player;
-//       }}
-//
-//       winPossibilities.forEach(function(possibility){
-//         if (possibility[0].dataset.value === "skull" && possibility[1].dataset.value === "skull" && possibility[2].dataset.value === "skull"){ console.log("Skulls Win!");
-//         return win = true;
-//
-//       } else if(possibility[0].dataset.value === "bones" && possibility[1].dataset.value === "bones" && possibility[2].dataset.value === "bones"){  console.log("Bones Win!");
-//       return win = true;
-//       }
-//
-//
-//
-//
-//
-//
-//
-//
-// })
-// }
-// //
-//   // Each square gets a click listener
-//   square.addEventListener('click', clickHandlerBoard);
-//
-//
-//
-// })
-// };
-//
-// })
+document.addEventListener('DOMContentLoaded', function(){
+
+// SET UP ARRAYS FROM TABLE TO calculate winPossibilities
+var row1var = document.getElementById("row1").children;
+var row2var = document.getElementById("row2").children;
+var row3var = document.getElementById("row3").children;
+var column1 = [row1var[0],row2var[0],row3var[0]];
+var column2 = [row1var[1],row2var[1],row3var[1]];
+var column3 = [row1var[2],row2var[2],row3var[2]];
+var diagLeft = [row1var[0],row2var[1],row3var[2]];
+var diagRight = [row3var[0],row2var[1],row1var[2]];
+var winPossibilities = [row1var,row2var,row3var, column1, column2, column3, diagLeft, diagRight];
+
+// GAME SET UP VARIABLES
+var resetButton = document.querySelector("button.reset");
+var moveCount = 0;
+var player = 1;
+
+// Game Board Variables
+var boardSquares = document.querySelectorAll('td.square');
+var board = document.querySelector("div#board");
+var playerTurn = document.querySelector("h2.player");
+var moves = document.querySelector("h2.moves");
+var win = false;
+var winner;
+var winning = document.createElement("div")
+var winningHeading = document.createElement("h1")
+
+// Game Board Counts/Stats
+function currentPlayer(player){
+  if (player === 1){
+    return "SKULLZ"
+  } else {
+    return "BOWNZ"
+  }
+};
+
+playerTurn.innerHTML = currentPlayer(player) + " MOVE";
+moves.innerHTML = "Total Moves: " + moveCount;
+
+boardSquares.forEach(function(square,index){
+
+  function squareClickHandler(event){
+    // Clear everything
+    event.stopPropagation;
+    event.preventDefault;
+
+    if (square.innerHTML === "" && win === false){
+      if (player === 1){
+        var skull = document.createElement('img');
+        skull.src = 'img/skull2.jpg';
+        square.setAttribute("data-value","skull")
+        square.append(skull);
+
+        moveCount++;
+        moves.innerHTML = "Total Moves: " + moveCount;
+        player = 2;
+        playerTurn.innerHTML = currentPlayer(player) + " MOVE";
+      } else if (player === 2){
+        var bones = document.createElement('img');
+        bones.src = 'img/bones.jpg';
+        square.setAttribute("data-value","bones")
+        square.append(bones);
+
+        moveCount++
+        moves.innerHTML = "Total Moves: " + moveCount;
+        player = 1;
+        playerTurn.innerHTML = currentPlayer(player) + " MOVE";
+      }
+      //end of if inner HTML empty
+    }
+    //end of if inner HTML empty
+    tie();
+    // After A Square is Filled Check for a Win
+    winPossibilities.forEach(function(possibility){
+      if (possibility[0].dataset.value === "skull" && possibility[1].dataset.value === "skull" && possibility[2].dataset.value === "skull"){ console.log("Skulls Win!");
+      possibility[0].style.border = "10px solid red";
+      possibility[1].style.border = "10px solid red";
+      possibility[2].style.border = "10px solid red";
+
+      winningHeading.innerHTML = 'Skullz WINS!!!';
+      var skullWinnerImage = skull.cloneNode(true);
+      skullWinnerImage.style.width = '30%';
+      winning.append(winningHeading);
+      winning.append(skullWinnerImage);
+      board.append(winning);
+
+      return win = true;
+
+    } else if(possibility[0].dataset.value === "bones" && possibility[1].dataset.value === "bones" && possibility[2].dataset.value === "bones"){  console.log("Bones Win!");
+    winner = "Bones";
+    possibility[0].style.border = "10px solid red";
+    possibility[1].style.border = "10px solid red";
+    possibility[2].style.border = "10px solid red";
+
+    winningHeading.innerHTML = 'Bones WINS!!!';
+    var boneswinnerImage = bones.cloneNode(true);
+    boneswinnerImage.style.width = '30%';
+    winning.append(winningHeading);
+    winning.append(boneswinnerImage);
+    board.append(winning);
+    return win = true;
+
+    }
+    // WIN POSSIBILITIES END
+  });
+  // WIN POSSIBILITIES END
+  function tie(){
+    if (moveCount === 9){
+
+      winningHeading.innerHTML = 'TIE';
+      winning.append(winningHeading);
+      board.append(winning);
+      return win = true;
+    }
+  }
+
+
+    // Square ClickHandler END
+  }
+  //
+
+// ADD EVENT LISTENER TO EACH SQUARE
+square.addEventListener('click', squareClickHandler);
+
+
+  // End of Squares Iteration
+})
+// End of Squares Iteration
+
+
+
+function resetGame(e){
+  console.log('reset clicked');
+
+  win = false;
+
+  boardSquares.forEach(function(square,index){
+    square.removeAttribute("data-value");
+    square.innerHTML = "";
+    square.classList.remove("winningSquare");
+    square.style = "";
+    winning.innerHTML = "";
+    winBox.style.display = "none";
+    moveCount = 0;
+    player = 1;
+    playerTurn.innerHTML = currentPlayer(player) + " MOVE";
+    moves.innerHTML = "Total Moves: " + moveCount;
+
+  })
+
+  // RESET GAME FUNCTION END
+}
+// RESET GAME FUNCTION END
+
+resetButton.addEventListener('click', resetGame);
+
+
+
+  //END OF DOMContentLoaded
+});
+// END OF DOMContentLoaded
